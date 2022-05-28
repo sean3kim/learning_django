1442b7587ee772ecd8eddac129e9d10e169f31d8
sean3kim <s3jkim@gmail.com>
2022-05-27 18:12:56 -0700

    once again

--- server/users/management/commands/createsu.py
+++ server/users/management/commands/createsu.py
@@ -1,9 +1,10 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
+from users.models import MyUser
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser("admin", "admin@admin.com", "admin")
+        if not MyUser.objects.filter(username="admin").exists():
+            MyUser.objects.create_superuser("admin", "admin@admin.com", "admin")
