6d8d6e1f5543713bd5f7972e1b91cf31dc76e83d
sean3kim <s3jkim@gmail.com>
2022-05-27 18:01:58 -0700

    something wrong in createsu custom command, trying to fix

--- server/server/management/commands/createsu.py
+++ server/server/management/commands/createsu.py
@@ -1,9 +1,9 @@
 from django.core.management.base import BaseCommand
-from users.models import MyUser
+from django.contrib.auth.models import User
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        if not MyUser.objects.filter(username="admin").exists():
-            MyUser.objects.create_superuser("admin", "admin@admin.com", "admin")
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser("admin", "admin@admin.com", "admin")
