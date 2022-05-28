7452c968386a7c9c4d7286e528e224f08e4b18c6
sean3kim <s3jkim@gmail.com>
2022-05-27 17:58:39 -0700

    superuser and staticfiles in eb

---
+++ server/server/management/commands/createsu.py
@@ -0,0 +1,9 @@
+from django.core.management.base import BaseCommand
+from users.models import MyUser
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        if not MyUser.objects.filter(username="admin").exists():
+            MyUser.objects.create_superuser("admin", "admin@admin.com", "admin")
